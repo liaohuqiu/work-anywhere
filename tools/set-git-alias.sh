c1b376ac4b5d2f8d7b2ab76eb229bc331cde0863
liaohuqiu <liaohuqiu@gmail.com>
2014-03-20 14:45:57 +0800

    update

--- tools/set-git-alias.sh
+++ tools/set-git-alias.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+. ../base.sh
+
+templatedir=$HOME/.git/templates
+hooks_dir=$templatedir/hooks
+mkdir -p $hooks_dir
+
+pre_commit_hook_file=$hooks_dir/pre-commit
+cp -f $root_dir/sample/git-hooks-pre-commit $pre_commit_hook_file
+chmod a+x $pre_commit_hook_file
+
+git config --global init.templatedir $templatedir
 git config --global alias.st 'status'
 git config --global alias.ci 'commit'
 git config --global alias.sb 'submodule'
