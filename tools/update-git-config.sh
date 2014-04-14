8adf62818fb44fa6085d5c89d6614c9b723b2847
liaohquiu <liaohuqiu@gmail.com>
2014-04-14 18:06:00 +0800

    fix bug: hooks can not be copied into denstiny directory

--- tools/update-git-config.sh
+++ tools/update-git-config.sh
@@ -5,7 +5,7 @@
 template_dir=$HOME/.git-templates
 tempalte_hooks_dir=$template_dir/hooks
 exe_cmd "mkdir -p $template_dir"
-exe_cmd "cp -rf $root_dir/sample/git-template/hooks/ $template_dir/"
+exe_cmd "cp -rf $root_dir/sample/git-template/hooks $template_dir/"
 exe_cmd "chmod -R a+x $tempalte_hooks_dir"
 
 exe_cmd "git config --global init.templatedir $template_dir"
