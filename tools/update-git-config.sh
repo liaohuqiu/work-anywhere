b6840d6662d3f7b895027e449e2c731b6c70a638
liaohuqiu <liaohuqiu@gmail.com>
2014-10-01 21:16:47 +0800

    alias git pm to git push origin master

--- tools/update-git-config.sh
+++ tools/update-git-config.sh
@@ -13,4 +13,5 @@ git config --global alias.st 'status'
 git config --global alias.ci 'commit'
 git config --global alias.sb 'submodule'
 git config --global alias.co 'checkout'
+git config --global alias.pm 'push origin master'
 git config --global alias.lg "log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset' --abbrev-commit"
