9f085b58af644b5c83fc130c49c8de868fe000f2
srain <srain@mac.local>
2015-04-03 12:15:07 +0800

    add git pd for git push origin dev

--- tools/update-git-config.sh
+++ tools/update-git-config.sh
@@ -15,6 +15,7 @@ git config --global alias.ci 'commit'
 git config --global alias.sb 'submodule'
 git config --global alias.co 'checkout'
 git config --global alias.pm 'push origin master'
+git config --global alias.pd 'push origin dev'
 git config --global alias.lg "log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.d difftool
 
