a48e72da647f8675c7f768ea36f887748f80b040
liaohuqiu <liaohuqiu@gmail.com>
2016-10-24 19:12:45 +0800

    add karabinder.sh

--- tools/update-git-config.sh
+++ tools/update-git-config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-. ../base.sh
+my_dir="$(dirname "$0")"
+my_dir="$(dirname "$my_dir")"
+
+. $my_dir/base.sh
 
 template_dir=$HOME/.git-templates
 tempalte_hooks_dir=$template_dir/hooks
@@ -18,6 +21,8 @@ git config --global alias.pm 'push origin master'
 git config --global alias.pd 'push origin dev'
 git config --global alias.lg "log --all --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ci) %C(bold blue)<%an>%Creset' --abbrev-commit"
 git config --global alias.d difftool
+
+git config --global alias.sm 'git branch --set-upstream-to=origin/master master'
 
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
---
+++ tools/update-karabiner.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set option.extra_cursor_optionL_kjhl 1
+/bin/echo -n .
+$cli set remap.controlL2fn 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f1 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f10 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f3_snow 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f5 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set remap.fn2controlL 1
+/bin/echo -n .
+/bin/echo
