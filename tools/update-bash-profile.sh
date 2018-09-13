b04b824f4eff090c256432d56cd7ca2b26be49c8
liaohqiu <liaohuqiu@gmail.com>
2018-09-13 15:04:55 +0800

    make update-bash-profile.sh works

--- tools/update-bash-profile.sh
+++ tools/update-bash-profile.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-my_dir="$(dirname "$0")"
-my_dir="$(dirname "$my_dir")"
 
-. $my_dir/base.sh
+___this_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+__root_dir="$(dirname $___this_dir)"
 
-current_dir=`pwd`
-root_dir=$(dirname $current_dir)
+. $__root_dir/base.sh
+
 bash_profile="$HOME/.bash_profile"
-sample_file_name="$root_dir/sample/bash-history-config"
+sample_file_name="$__root_dir/sample/bash-history-config"
 tag_str="tag_bash_history"
 
 exe_cmd "copy_from_sample $bash_profile $sample_file_name $tag_str"
